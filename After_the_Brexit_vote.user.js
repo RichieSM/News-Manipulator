30d4301f1e447e961ecf9e2adb3f332b63142f90
Richard Mahoney <richard.mahoney@rackspace.co.uk>
2016-10-19 20:31:32 +0100

    Adding Brexit

---
+++ After_the_Brexit_vote.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name        After the Brexit vote
+// @namespace   After the Brexit vote
+// @description Adds " after the Brexit vote" to every title on the BBC News UK homepage
+// @category    Humor/Satire
+// @include     http://www.bbc.co.uk/news
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+// Options
+
+// Emoji options
+var emoji = true;
+var keyword = 'Brexit';
+var emoji_before_keyword = '🇬🇧'; // TODO: use character encoding values
+var emoji_after_keyword = '🍺';
+
+// Code
+
+var bbctitle = document.body.getElementsByClassName('title-link__title-text');
+var i;
+for (i = 0; i < bbctitle.length; i++) {
+  if (emoji) {
+    bbctitle[i].innerHTML = bbctitle[i].innerHTML + ' after the ' + emoji_before_keyword + ' ' + keyword + ' ' + emoji_after_keyword + ' vote';
+  }
+  else {
+    bbctitle[i].innerHTML = bbctitle[i].innerHTML + ' after the ' + keyword + ' vote';
+  }
+  
+}
